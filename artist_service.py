47affe519fe2c4ea1d88ddfcca8ca254dee56d4a
Isaiah <anyiminene@gmail.com>
2018-08-08 10:47:14 -0700

    added content

---
+++ artist_service.py
@@ -0,0 +1,22 @@
+artist_info = {
+    'Future': {
+        'image':'future.jpg',
+        'spotify_link':'1RyvyyTE3xzB2ZywiAwp0i'
+    },
+    'Young Thug': {
+        'image':'young_thug.jpg',
+        'spotify_link':'50co4Is1HCEo8bhOyUWKpn'
+    },
+    '21 Savage': {
+        'image':'savage.jpg',
+        'spotify_link':'1URnnhqYAYcrqrcwql10ft'
+    },
+    'Travis Scott': {
+        'image':'travis_scott.jpg',
+        'spotify_link':'0Y5tJX1MQlPlqiwlOH1tJY'
+    },
+    'Lil Wayne': {
+        'image':'lil_wayne.jpg',
+        'spotify':'55Aa2cqylxrFIXC767Z865'
+    }
+}
---
+++ song_service.py
@@ -0,0 +1,31 @@
+song_info = {
+    'Blow a Bag': {
+        'image':'future.jpg',
+        'spotify_link':'6ycYqFnXnjMOwMvEMbEpUk'
+    },
+    'With Them': {
+        'image':'young_thug.jpg',
+        'spotify_link':'0tISnxqgVmxqhVghsTi2Rr'
+    },
+    'Money Convo': {
+        'image':'savage.jpg',
+        'spotify_link':'54i1CQFlT3LeGdIVctDQp1'
+    },
+    'STARGAZING': {
+        'image':'travis_scott.jpg',
+        'spotify_link':'7wBJfHzpfI3032CSD7CE2m'
+    },
+    'No Worries': {
+        'image':'lil_wayne.jpg',
+        'spotify':'2DLsQuwxEdXt2yp54vw355'
+    }
+}
+<iframe src="https://open.spotify.com/embed/track/6ycYqFnXnjMOwMvEMbEpUk" width="300" height="380" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>
+
+<iframe src="https://open.spotify.com/embed/track/0tISnxqgVmxqhVghsTi2Rr" width="300" height="380" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>
+
+<iframe src="https://open.spotify.com/embed/track/54i1CQFlT3LeGdIVctDQp1" width="300" height="380" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>
+
+<iframe src="https://open.spotify.com/embed/track/7wBJfHzpfI3032CSD7CE2m" width="300" height="380" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>
+
+<iframe src="https://open.spotify.com/embed/track/2DLsQuwxEdXt2yp54vw355" width="300" height="380" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>
